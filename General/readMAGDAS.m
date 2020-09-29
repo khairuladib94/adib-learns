9e4a952f5afd27c6c68efda340fb9713f04dd091
Adib Yusof <mkhairuladibmyusof@gmail.com>
2020-09-29 20:51:53 +0800

    Add backslash to filepath and change string to char automatically

--- General/readMAGDAS.m
+++ General/readMAGDAS.m
@@ -2,16 +2,19 @@ function GM = readMAGDAS(StartDate, EndDate, StnCode, SamplingPeriod, FileNameFo
 
 % This function reads MAGDAS geomagnetic field data
 %
-% Examples of input arguments
+% Output argument
+%   GM (table)                                  : Extracted geomagnetic field data                              
 %
-%   StartDate                = [2012, 01, 20]
-%   EndDate                  = [2012, 01, 27]
-%   StnCode                  = 'CEB'
-%   SamplingPeriod (seconds) = 1
-%   FilePath                 = 'D:\OneDrive\Belajar\Sandboxes\Learning\General\Raw data\'
-%   FileNameFormat           = 'psec.sec'
-%   DataFormat (Optional)    = '%4d-%02d-%02d %02d:%02d:%02d.000 %3d %9.2f %9.2f %9.2f %9.2f'
-%   HeaderLine (Optional)    = 13
+% Input arguments (with examples)
+%
+%   StartDate (3x1 double)                      : [2012, 01, 20]
+%   EndDate (3x1 double)                        : [2012, 01, 27]
+%   StnCode (3x1 char)                          : 'CEB'
+%   SamplingPeriod (in seconds | 1x1 double)    : 1
+%   FolderPath (char vector)                    : 'D:\OneDrive\Belajar\Sandboxes\Learning\General\Raw data\'
+%   FileNameFormat (char vector)                : 'psec.sec'
+%   DataFormat (optional | char vector)         : '%4d-%02d-%02d %02d:%02d:%02d.000 %3d %9.2f %9.2f %9.2f %9.2f' (default)
+%   HeaderLine (optional | 1x1 double)          : 13 (default)
 %
 % Written by Adib Yusof (2020) | mkhairuladibmyusof@gmail.com
 
@@ -21,16 +24,21 @@ arguments
     StnCode (1,3) char
     SamplingPeriod (1,1) double
     FileNameFormat (1,:) char
-    FolderPath (1,:) char = [pwd, '\']
+    FolderPath (1,:) char = pwd
     DataFormat (1,:) char = '%4d-%02d-%02d %02d:%02d:%02d.000 %3d %9.2f %9.2f %9.2f %9.2f'
     HeaderLine (1,1) double = 13
 end
 
+tic
+StnCode = char(StnCode); FileNameFormat = char(FileNameFormat); FolderPath = char(FolderPath); DataFormat = char(DataFormat);
 UTC(:, 1) = datetime(StartDate) : seconds(SamplingPeriod) : datetime(EndDate) + days(1) - seconds(1);
 [H, D, Z, F] = deal( NaN(numel(UTC), 1) );
 DataIdx = 1;
 IndxIncrement = (86400 / SamplingPeriod) - 1;
 FileNameFormat = ['%3s%04d%02d%02d', FileNameFormat];
+if FolderPath(end) ~= '\'
+    FolderPath = [FolderPath, '\'];
+end
 
 for i = datetime(StartDate) : datetime(EndDate)
     DateAsVector = datevec(i);
@@ -38,11 +46,10 @@ for i = datetime(StartDate) : datetime(EndDate)
     FullFilePath = [FolderPath, FileName];
     FileID = fopen(FullFilePath);
     if FileID < 0
-        disp([FileName, ' not found']);
         continue;
     end
     
-    disp(['Extracting ', FileName]);
+    disp([FileName, ' has been extracted.']);
     ExtractedData = textscan(FileID, DataFormat, 'HeaderLines', HeaderLine);
     [H(DataIdx:DataIdx+IndxIncrement), D(DataIdx:DataIdx+IndxIncrement), Z(DataIdx:DataIdx+IndxIncrement), F(DataIdx:DataIdx+IndxIncrement)] = ExtractedData{end-3:end};
     
@@ -51,5 +58,6 @@ for i = datetime(StartDate) : datetime(EndDate)
 end
 
 GM = table(UTC, H, D, Z, F);
-GM.Properties.Description = [StnCode, ' data obtained from MAGDAS'];
+GM.Properties.Description = [StnCode, ' station data obtained from MAGDAS.'];
+fprintf('\nExtraction finished after %.2f seconds.\n', toc);
 end
